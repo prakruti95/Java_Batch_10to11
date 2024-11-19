5d7f060be2f3856085f7f10e60370feee6500fa7
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-11-19 11:06:43 +0530

    commit

---
+++ Module1/19-11/Ex2.java
@@ -0,0 +1,20 @@
+
+public class Ex2 
+{
+	public static void main(String[] args) 
+	{
+		
+		int marks=27;//variable declaration
+		
+		//conditions check
+		if(marks>35)
+		{
+			System.out.println("pass");//output1
+		}
+		else
+		{
+			System.out.println("fail");//output2
+		}
+		
+	}
+}
