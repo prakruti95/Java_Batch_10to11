da8ffaed5d7200eee6ecc0aca4913a9539a1777d
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-01-24 16:04:48 +0530

    commit

---
+++ Module4/24-1/ServletContextConfigEx/src/main/java/ContextEx.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ContextEx")
+public class ContextEx extends HttpServlet 
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		ServletContext context = getServletContext();
+		
+		out.print(context.getInitParameter("Email"));
+		
+		
+		
+	}
+}
