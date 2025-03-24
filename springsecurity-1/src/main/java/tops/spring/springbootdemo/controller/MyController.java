33efea93ca2012611b58aef179408f569086eb4f
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-03-24 11:20:29 +0530

    commit

---
+++ springsecurity-1/src/main/java/tops/spring/springbootdemo/controller/MyController.java
@@ -0,0 +1,20 @@
+package tops.spring.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class MyController 
+{
+	@RequestMapping("/")
+	public String index()
+	{
+	
+		
+		return "index";
+	}
+	
+}
---
+++ springsecurity-1/src/test/java/tops/spring/springbootdemo/Springsecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tops.spring.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
